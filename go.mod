6c119ea32fc0f9a94ccfeaa55d4f38ab20535c6e
Tochukwu Victor <tvictor@optoro.com>
2020-11-25 02:04:26 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tochukwuvictor/go-logHelpers
+
+go 1.14
+
+require github.com/hashicorp/go-hclog v0.15.0
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"errors"
+
+	log "github.com/hashicorp/go-hclog"
+)
+
+// SetupLogger returns a logger initialized with the log level and program name
+func SetupLogger(logLevel, programName string) (logger log.Logger, err error) {
+	level := log.LevelFromString(logLevel)
+	if level == 0 {
+		return logger, errors.New("Invalid log level specified: " + logLevel)
+	}
+
+	return log.New(&log.LoggerOptions{
+		Name:  programName,
+		Level: level,
+	}), nil
+}
